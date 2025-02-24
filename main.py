5d602e1bba29feaa857f1f416b68b4ce4bc5ebeb
BaojunJiang <baojun.jiang@edetek.com>
2025-02-24 18:15:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+@app.get("/")
+async def hello():
+    return RedirectResponse(url="/static/index.html", status_code=301)
+    # return "hello"
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=False)
